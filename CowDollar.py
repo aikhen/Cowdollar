540f3eab3e0e21a281f1f57036085b4a41f83299
aikhen <52520856+aikhen@users.noreply.github.com>
2019-07-18 04:41:54 +0800

    Cowdollar

--- CowDollar.py
+++ CowDollar.py
@@ -47,7 +47,7 @@ ua = {
 }
 
 c = requests.session()
-r = c.get("https://gymx.site/en/login",headers=ua)
+r = c.get("https://gymx.me/en/login",headers=ua)
 soup = BeautifulSoup(r.text,"html.parser")
 a = 0
 for auth in soup.findAll("input"):
@@ -56,7 +56,7 @@ for auth in soup.findAll("input"):
     if a == 2:
       break
 
-r = c.post("https://gymx.site/en/login",headers=ua,data={"utf8": "&#x2713;","authenticity_token": autho,"user[email]": my_namespace.email,"user[password]": my_namespace.password,"commit": "Login"})
+r = c.post("https://gymx.me/en/login",headers=ua,data={"utf8": "&#x2713;","authenticity_token": autho,"user[email]": my_namespace.email,"user[password]": my_namespace.password,"commit": "Login"})
 soup = BeautifulSoup(r.text,"html.parser")
 print ("\033[1;37m"+soup.title.text,"\n")
 a =0
@@ -86,16 +86,16 @@ print ("\033[0;34m\n\n========================================================="
 print ("\033[1;37m\n\nLet's Start Mining......!")
 while True:
  try:
-  r = c.get("https://gymx.site/en/mining/mine",headers=ua,cookies=r.cookies,timeout=15)
-  r1 = c.post("https://gymx.site/mining/toggle_miner_state/",headers={"accept": "application/json, text/javascript, */*; q=0.01","x-csrf-token": token,"x-requested-with": "XMLHttpRequest","user-agent": "Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36","content-type": "application/x-www-form-urlencoded; charset=UTF-8","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"} ,data={"state[mining_state]": "actived"},cookies=r.cookies,timeout=15)
+  r = c.get("https://gymx.me/en/mining/mine",headers=ua,cookies=r.cookies,timeout=15)
+  r1 = c.post("https://gymx.me/mining/toggle_miner_state/",headers={"accept": "application/json, text/javascript, */*; q=0.01","x-csrf-token": token,"x-requested-with": "XMLHttpRequest","user-agent": "Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36","content-type": "application/x-www-form-urlencoded; charset=UTF-8","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"} ,data={"state[mining_state]": "actived"},cookies=r.cookies,timeout=15)
   j = json.loads(r1.text)
   sys.stdout.write("\r\033[1;30m# \033[1;32m"+j["message"]["title"])
   tunggu(int(my_namespace.sleep))
-  r2 = c.post("https://gymx.site/earnings",headers={"accept": "application/json, text/javascript, */*; q=0.01","x-csrf-token": token,"x-requested-with": "XMLHttpRequest","user-agent": "Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36","content-type": "application/x-www-form-urlencoded; charset=UTF-8","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"},data={"earning[bitcoin]": "0.0000005"},cookies=r.cookies,timeout=15)
+  r2 = c.post("https://gymx.me/earnings",headers={"accept": "application/json, text/javascript, */*; q=0.01","x-csrf-token": token,"x-requested-with": "XMLHttpRequest","user-agent": "Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36","content-type": "application/x-www-form-urlencoded; charset=UTF-8","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"},data={"earning[bitcoin]": "0.0000005"},cookies=r.cookies,timeout=15)
   js = json.loads(r2.text)
   sys.stdout.write("\r\033[1;30m#\033[0;32m "+js["message"]["title"]+" "+js["message"]["msg"])
-  r3 = c.post("https://gymx.site/mining/toggle_miner_state/",headers={"accept": "application/json, text/javascript, */*; q=0.01","x-csrf-token": token,"x-requested-with": "XMLHttpRequest","user-agent": "Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36","content-type": "application/x-www-form-urlencoded; charset=UTF-8","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"},data={"state[mining_state]": "passived"},cookies=r.cookies,timeout=15)
-  r4 = c.get("https://gymx.site/en/mining/dashboard",headers=ua,cookies=r.cookies,timeout=15)
+  r3 = c.post("https://gymx.me/mining/toggle_miner_state/",headers={"accept": "application/json, text/javascript, */*; q=0.01","x-csrf-token": token,"x-requested-with": "XMLHttpRequest","user-agent": "Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/72.0.3626.121 Mobile Safari/537.36","content-type": "application/x-www-form-urlencoded; charset=UTF-8","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"},data={"state[mining_state]": "passived"},cookies=r.cookies,timeout=15)
+  r4 = c.get("https://gymx.me/en/mining/dashboard",headers=ua,cookies=r.cookies,timeout=15)
   soup = BeautifulSoup(r4.text,"html.parser")
   sys.stdout.write("\r\033[1;30m# \033[1;32mYour BTC Balance\033[1;31m :\033[1;32m "+soup.find("span", class_="counter") .text+"\n")
  except:
